d90c74d2da7414bd6538792c9aa86f6d76dfaaf2
Dr. Safi <sfmissive@gmail.com>
2018-04-18 18:55:32 +0300

    Converted require to dynamic imports.

--- source/databaseData/databaseData.js
+++ source/databaseData/databaseData.js
@@ -1,25 +1,28 @@
-let middlewareArray = require('./webappSetting/middleware')
-let templateArray = require('./webappSetting/template')
-let conditionArray = require('./webappSetting/condition')
-let oAuthArray = require('./webappSetting/oAuth')
-let schemaArray = require('./webappSetting/schema')
 
-let webappSetting = [].concat.apply([], [
-    middlewareArray, 
-    templateArray,
-    conditionArray,
-    oAuthArray,
-    schemaArray
-])
-
-export default {
-    webappSetting, 
-    webappContent: [
-        require('./webappContent/personalInfo.js'),
-        require('./webappContent/ui.js'),
-        require('./webappContent/article.js'),
-        require('appscript/databaseData/content/language.js'),
-        require('appscript/databaseData/content/relationship.js'),
-        require('appscript/databaseData/content/country.js')
-    ]
+export default async () => {
+    let middlewareArray = await import('./webappSetting/middleware')
+    let templateArray = await import('./webappSetting/template')
+    let conditionArray = await import('./webappSetting/condition')
+    let oAuthArray = await import('./webappSetting/oAuth')
+    let schemaArray = await import('./webappSetting/schema')
+    
+    let webappSetting = [].concat.apply([], [
+        middlewareArray, 
+        templateArray,
+        conditionArray,
+        oAuthArray,
+        schemaArray
+    ])
+    
+    return {
+        webappSetting, 
+        webappContent: [
+            await import('./webappContent/personalInfo.js'),
+            await import('./webappContent/ui.js'),
+            await import('./webappContent/article.js'),
+            await import('appscript/databaseData/content/language.js'),
+            await import('appscript/databaseData/content/relationship.js'),
+            await import('appscript/databaseData/content/country.js')
+        ]
+    }
 }
--- source/serverSide/app.js
+++ source/serverSide/app.js
@@ -4,8 +4,10 @@ import configuration from '../../setup/configuration/configuration.js'
 import { microservice } from 'appscript'
 import databaseData from '../databaseData/databaseData.js'
 
-microservice({
-    configuration,
-    entrypointConditionKey: '78f91938-f9cf-4cbf-9bc8-f97836ff23dd',
-    databaseData
-})
+;(async () => {
+    microservice({
+        configuration,
+        entrypointConditionKey: '78f91938-f9cf-4cbf-9bc8-f97836ff23dd',
+        databaseData: await databaseData()
+    })
+})()
