422eaf10e08e5e5906f31e3f9df0f016cffbe8ab
Dr. Safi <sfmissive@gmail.com>
2018-05-21 03:22:03 +0300

    Request polyfilled version using context request params/query.

--- source/serverSide/app.js
+++ source/serverSide/app.js
@@ -10,4 +10,4 @@ import databaseData from '../databaseData/databaseData.js'
         entrypointConditionKey: '78f91938-f9cf-4cbf-9bc8-f97836ff23dd',
         databaseData: await databaseData()
     })
-})()
+})() 
