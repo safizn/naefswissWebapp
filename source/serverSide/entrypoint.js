ea4f0775c0ca0fc8b6c4b4c3b29a9728cbabafb2
Dr. Safi <sfmissive@gmail.com>
2018-07-17 05:22:55 +0300

    Abstract common functionality in separate modules.

--- source/serverSide/entrypoint.js
+++ source/serverSide/entrypoint.js
@@ -1,20 +1,11 @@
-const moduleSystem = require('module')
-const path = require('path')
-const configuration = require('../../setup/configuration/configuration.js')
+const   path = require('path'),
+        configuration = require('../../setup/configuration/configuration.js'),
+        { addModuleResolutionPath } = require('./utility/addModuleResolutionPath.js'),
+        babelJSTranspilerPath = path.normalize(`${configuration.directory.appDeploymentLifecyclePath}/babel_javascriptTranspilation.js/entrypoint.js`)
 
 /** Set any environment variables before loading rest of the modules - this is done only in the entrypoint, since babel transforms the code */
 process.env.SZN_DEBUG = true // show/hide console messages.
 process.env.DEBUG = 'oidc-provider:*'
-
-
-const appRootPath = path.normalize(__dirname)
-// add root path (app base path) to the resolved module paths.
-// Define server base path. Hackish way to make sure the path is always consistent. Base path in Nodejs is where the closest parent node_modules is located to the initiated js script.
-// '${appRootPath}' allows for folders/modules inside the main folder to be called with out using relative paths.
-// '${appRootPath}/node_modules' allows for modules from upper herarchies to call modules from sibling folders. e.g. source/x calls source/y/node_modules/module
-process.env.NODE_PATH = `${process.env.NODE_PATH || ''}:${appRootPath}:${appRootPath}/node_modules`.replace(/(^\:+)/, '')
-console.log(`• Node additional module resolution paths: ${process.env.NODE_PATH}`)
-moduleSystem._initPaths()
 
 switch (process.env.DEPLOYMENT) {
     case 'production':
@@ -24,8 +15,7 @@ switch (process.env.DEPLOYMENT) {
         // Transpile js scripts on runtime using Babel.
         // global.SZN = {}
         // global.SZN.APP = require('appscript/configuration/configuration.export.js') // Load configuration settings. NOTE: babel doesn't order import correctly when compiling, therefore global.SZN is required in this file not in app.js.
-        const babelJSCompilerPath = path.normalize(`${configuration.directory.appDeploymentLifecyclePath}/babel_javascriptTranspilation.js/entrypoint.js`)
-        const babelJSCompiler = require(babelJSCompilerPath)
+        const babelJSCompiler = require(babelJSTranspilerPath)
         babelJSCompiler({
             babelConfigurationFile: 'serverRuntime.BabelConfig.js'
         })
